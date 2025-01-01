f9be9f4613a938f6eceb957608ec48f2894752b4
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-01-01 21:47:40 +0200

    Q1- Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.

---
+++ Assignment/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Distance(Point point2)
+        {
+            double dx = X - point2.X;
+            double dy = Y - point2.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Assignment
+﻿using System.Transactions;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1- Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+            //Person[] Persons = new Person[3];
+            //for (int i = 0; i < Persons.Length; i++)
+            //{
+            //    Console.WriteLine($"Enter Person {i + 1} Data => ");
+            //    Console.Write($"Person {i + 1} Name => ");
+            //    Persons[i].Name = Console.ReadLine()!;
+            //    Console.Write($"Person {i + 1} Age => ");
+            //    int.TryParse(Console.ReadLine(), out int age);
+            //    Persons[i].Age = age;
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine("Persons Data => ");
+            //for (int i = 0; i < Persons.Length; i++)
+            //{
+            //    Console.WriteLine($"person {i + 1} Name => {Persons[i].Name}");
+            //    Console.WriteLine($"person {i + 1} Age => {Persons[i].Age}");
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            
         }
     }
 }
