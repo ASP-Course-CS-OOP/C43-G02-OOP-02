024e35b8d476e7a452754e096466dbca6dee07a8
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-01-01 21:56:45 +0200

    Q2- Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -28,7 +28,28 @@ namespace Assignment
             //}
             #endregion
 
-            
+            #region 2- Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+            //Console.Write("Enter coordinate(X) of point 1: ");
+            //double x1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter coordinate(Y) of point 1: ");
+            //double y1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine();
+
+            //Console.Write("Enter coordinate(X) of point 2: ");
+            //double x2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter coordinate(Y) of point 2: ");
+            //double y2 = Convert.ToDouble(Console.ReadLine());
+
+            //Point p1 = new Point(x1, y1);
+            //Point p2 = new Point(x2, y2);
+
+            //double distance = p1.Distance(p2);
+
+            //Console.WriteLine();
+
+            //Console.WriteLine($"Distance between the points: {distance:F3}");
+            #endregion
         }
     }
 }
